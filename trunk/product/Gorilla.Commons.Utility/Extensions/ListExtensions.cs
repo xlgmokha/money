bec3cabd03539c193548e5824a336597db118ec3
mokhan <mokhan@ce5e1baf-6525-42e4-a1b2-857ea38da20a>
2009-04-24 15:53:12 +0000

    git-svn-id: https://svn.xp-dev.com/svn/mokhan-mo.money@191 ce5e1baf-6525-42e4-a1b2-857ea38da20a

--- trunk/product/Gorilla.Commons.Utility/Extensions/ListExtensions.cs
+++ trunk/product/Gorilla.Commons.Utility/Extensions/ListExtensions.cs
@@ -6,12 +6,12 @@ namespace Gorilla.Commons.Utility.Extensions
 {
     static public class ListExtensions
     {
-        static public IListConstraint<T> add<T>(this IList<T> items, T item)
+        static public IListConstraint<T> add<T>(this ICollection<T> items, T item)
         {
             return new ListConstraint<T>(items, item);
         }
 
-        static public IListConstraint<T> add_range<T>(this IList<T> items, IEnumerable<T> item)
+        static public IListConstraint<T> add_range<T>(this ICollection<T> items, IEnumerable<T> item)
         {
             return new ListConstraint<T>(items, item.ToArray());
         }
@@ -19,10 +19,10 @@ namespace Gorilla.Commons.Utility.Extensions
 
     public class ListConstraint<T> : IListConstraint<T>
     {
-        readonly IList<T> items;
+        readonly ICollection<T> items;
         readonly T[] items_to_add;
 
-        public ListConstraint(IList<T> list_to_constrain, params T[] items_to_add)
+        public ListConstraint(ICollection<T> list_to_constrain, params T[] items_to_add)
         {
             items = list_to_constrain;
             this.items_to_add = items_to_add;
