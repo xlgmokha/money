c155591e2621c7f2381d2ae5bb7b1d9c747c9ea8
mo khan <mo@mokhan.ca>
2010-02-02 20:26:58 -0700

    created tests project.

--- product/client/presentation/Model/Navigation/IBranchVisitor.cs
+++
@@ -1,6 +0,0 @@
-using gorilla.commons.utility;
-
-namespace MoMoney.Presentation.Model.Navigation
-{
-    public interface IBranchVisitor : Visitor<ITreeBranch> {}
-}
--- product/client/presentation/Model/Navigation/INavigationTreeVisitor.cs
+++
@@ -1,7 +0,0 @@
-using System.Windows.Forms;
-using gorilla.commons.utility;
-
-namespace MoMoney.Presentation.Model.Navigation
-{
-    public interface INavigationTreeVisitor : Visitor<TreeView> {}
-}
