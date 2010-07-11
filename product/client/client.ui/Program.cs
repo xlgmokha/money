36102d4a076961db17d5ca2ba16ceb0c65d3c298
mo khan <mo@mokhan.ca>
2010-07-10 18:48:39 -0600

    updated run target to allow for running app from commandline. removed unused tools.

--- product/client/client.ui/Program.cs
+++ product/client/client.ui/Program.cs
@@ -12,9 +12,9 @@ namespace presentation.windows
     static public class Program
     {
         [STAThread]
-        static public void Main()
+        static public void Main(string[] args)
         {
-            Process.Start(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\server\bin\Debug\presentation.windows.server.exe")));
+            Process.Start(get_startup_path_using(args));
             AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
             Dispatcher.CurrentDispatcher.UnhandledException += (o, e) =>
             {
@@ -24,6 +24,13 @@ namespace presentation.windows
             {
                 ShutdownMode = ShutdownMode.OnMainWindowClose
             }.Run(Bootstrapper.create_window());
+        }
+
+        static string get_startup_path_using(string[] args)
+        {
+            if (args.Length > 0)
+                return Path.Combine(args[0], @"presentation.windows.server.exe");
+            return Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"..\..\..\server\bin\Debug\presentation.windows.server.exe"));
         }
     }
 }
